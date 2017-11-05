6ae12a04a05f3763274579d295af7e0a978a0f72
yogpande <yogpande@users.noreply.github.com>
2017-11-05 21:46:23 +0530

    Add files via upload
    fsdfsdf

---
+++ WebApiApp/Controllers/ApiiController.cs
@@ -0,0 +1,24 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Web.Http;
+using WebApiApp.Models;
+
+namespace WebApiApp.Controllers
+{
+    public class ApiiController : ApiController
+    {
+
+        protected readonly DBEntities UserDB = new DBEntities();
+        protected HttpResponseMessage ToJson(dynamic obj)
+        {
+            var response = Request.CreateResponse(HttpStatusCode.OK);
+            response.Content = new StringContent(JsonConvert.SerializeObject(obj), Encoding.UTF8, "application/json");
+            return response;
+        }
+    }
+}
